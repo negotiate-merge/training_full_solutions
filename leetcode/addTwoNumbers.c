bfef38d4c34ab2fe2f1c3ca258ceadff024186d0
Nigel <nigel@synergitech.com.au>
2024-10-24 14:51:21 +1100

    updated to handle all the test cases, jumped the gun on the earlier commit

--- leetcode/addTwoNumbers.c
+++ leetcode/addTwoNumbers.c
@@ -19,8 +19,8 @@ struct ListNode* reverse(struct ListNode** l1);
 
 
 int main(void) {
-  int nums1[] = {2, 4, 3};
-  int nums2[] = {5, 6, 4};
+  int nums1[] = {9,9,9,9,9,9,9};  //{2, 4, 3};
+  int nums2[] = {9,9,9,9};  //{5, 6, 4};
   ln* lists[LIST_OBJS];
   
   lists[0] = buildList(nums1, sizeof(nums1) / sizeof(nums1[0]));
@@ -37,54 +37,38 @@ int main(void) {
 
 
 struct ListNode* addTwoNumbers(struct ListNode** l1, struct ListNode** l2) {
-  // Reverse the lists
-  *l1 = reverse(l1);
-  *l2 = reverse(l2);
+  // Allocate the initial memory 
+  struct ListNode *result = malloc(sizeof(struct ListNode));
+  struct ListNode *cur = result;
 
-  struct ListNode* p1 = *l1;
-  struct ListNode* p2 = *l2;
+  struct ListNode *p1 = *l1;
+  struct ListNode *p2 = *l2;
 
-  ln* head = (ln*) malloc(sizeof(ln));
-  ln* cur = head;
   int carry = 0;
-  int insert;
 
-  while (p1 != NULL || p2 != NULL) {
-    insert = p1->val + p2->val;
-    insert += carry;
-    carry = insert >= 10 ? 1 : 0;
-    insert = insert % 10;
-
-    cur->val = insert;
-    if (p1->next == NULL) {
-      cur->next = NULL;
-    } else {
-      cur->next = (struct ListNode*) malloc(sizeof(struct ListNode));
-      cur = cur->next;
+  while (p1 || p2 || carry) {
+    if (p1) {
+      printf("p1->val is %d\t", p1->val);
+      carry += p1->val;
+      p1 = p1->next;
     }
-    p1 = p1->next;
-    p2 = p2->next;
-  }
-
-  printf("Exiting addTwoNumbers\n\n");
-  return head;
+    if (p2) {
+      printf("p2->val is %d\t", p2->val);
+      carry += p2->val;
+      p2 = p2->next;
+    }
+    printf("carry is %d\n", carry);
+    cur->val = carry % 10;
+    carry /= 10;
+    if (p1 || p2 || carry) {
+      cur->next = malloc(sizeof(struct ListNode));
+      cur = cur->next;
+    } else {
+      cur->next = NULL;
+    }
+  } 
+  return result;
 } // End addTwoNumbers
-
-
-struct ListNode* reverse(struct ListNode** l1) {
-  struct ListNode *prev = NULL;
-  struct ListNode *current = *l1;
-  struct ListNode *next = NULL;
-
-  while (current != NULL) {
-    next = current->next;   // Save the next node
-    current->next = prev;   // Reverse the current node's pointer
-    prev = current;         // Move prev to current node
-    current = next;         // Move to the next node
-  }
-  
-  return prev;  // The new head of the reversed list
-}
 
 
 void printList(ln* list) {
@@ -126,3 +110,4 @@ ln* buildList(int* nums, int size) {
   }
   return head;
 } // End buildList
+
